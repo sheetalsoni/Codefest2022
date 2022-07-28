96c9001618d6531a1a94ac9255065daa39e456cb
sheetal soni <sheetal.soni@hotmail.com>
2022-07-27 21:45:22 -0500

    Home Page changes

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["On Time", "Before Time", "Delayed"],
     datasets: [{
       data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      backgroundColor: ['#f6c23e', '#56821f', '#CF4520'],
+      hoverBackgroundColor: ['#f6c23e', '#56821f', '#CF4520'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
